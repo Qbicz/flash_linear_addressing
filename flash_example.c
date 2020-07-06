4e1ebd2e80b900973d1bb2b319a77852f6bed677
Filip Kubicz <filip@kubicz.engineer>
2020-07-06 09:36:48 +0200

    flash_example: Add example of using init() and write()

---
+++ flash_example.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "flash/flash.h"
+
+void user_callback(ErrorCode_t code)
+{
+    printf("callback: error %d\n", code);
+}
+
+int main(void)
+{
+    // printf only used for demo/testing purpose, on microcontroller disable stdio.h and use dedicated logging
+    printf("Flash example start\n");
+
+    uint32_t page_number = 0;
+    S_Array_t data_to_write = {0};
+
+    FlashDriver_Init(user_callback);
+    ErrorCode_t err = FlashDriver_Write(page_number, data_to_write, user_callback);
+    if (err != SUCCESS)
+    {
+        printf("Flash write failed with code %d\n", err);
+    }
+
+    return 0;
+}
+
